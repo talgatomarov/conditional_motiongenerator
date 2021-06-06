16463313f90895ab565df0473dceb5b472ce40cf
Talgat Omarov <talgat.omarov.1999@gmail.com>
2021-06-06 16:37:27 +0600

    Add ArgumentParser

--- train.py
+++ train.py
@@ -1,17 +1,28 @@
+from argparse import ArgumentParser
 from .model import MotionGeneratorDataModule, MotionGenerator
 from transformers import T5TokenizerFast as T5Tokenizer
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 
 if __name__ == "__main__":
-    tokenizer = T5Tokenizer.from_pretrained("t5-small")
-    datamodule = MotionGeneratorDataModule(batch_size=32, tokenizer=tokenizer)
+    parser = ArgumentParser()
+
+    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--model_name", type=str, default="t5-small")
+    parser.add_argument("--max_epochs", type=int, default=10)
+    parser.add_argument("--gpus", type=int, default=0)
+    parser.add_argument("--save_top_k", type=int, default=1)
+
+    args = parser.parse_args()
+
+    tokenizer = T5Tokenizer.from_pretrained(args.model_name)
+    datamodule = MotionGeneratorDataModule(batch_size=args.batch_size, tokenizer=tokenizer)
     model = MotionGenerator()
 
     checkpoint_callback = ModelCheckpoint(
         dirpath="checkpoints",
         filename="best-checkpoint",
-        save_top_k=1,
+        save_top_k=args.save_top_k,
         verbose=True,
         monitor="val_loss",
         mode="min"
@@ -19,8 +30,8 @@ if __name__ == "__main__":
 
     trainer = pl.Trainer(
         checkpoint_callback=checkpoint_callback,
-        max_epochs=50,
-        gpus=1, 
+        max_epochs=args.max_epochs,
+        gpus=args.gpus, 
         progress_bar_refresh_rate=5
     )
 
